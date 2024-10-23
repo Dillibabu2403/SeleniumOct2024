d928c8ca4217241ba9cac2b80eb5e48a2a2511c3
Dillibabu2403 <118703394+Dillibabu2403@users.noreply.github.com>
2024-10-23 18:09:04 +0530

    Add files via upload
    week2day3assignments

---
+++ day3/TestData.java
@@ -0,0 +1,10 @@
+package week2.day3;
+
+public class TestData {
+public void enterCredentials() {
+	System.out.println("Credentials");
+}
+public void navigateToHomePage() {
+	System.out.println("Home Page");
+}
+}
