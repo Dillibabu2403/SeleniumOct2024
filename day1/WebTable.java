df445b058412070b3d9b8441f36ebfc7cbdbe079
Dillibabu2403 <118703394+Dillibabu2403@users.noreply.github.com>
2024-11-05 09:20:19 +0530

    Add files via upload
    week4day1assignments

---
+++ day1/WebTable.java
@@ -0,0 +1,35 @@
+package week4.day1;
+
+import java.time.Duration;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class WebTable {
+
+	public static void main(String[] args) {
+		 ChromeDriver driver = new ChromeDriver();
+			driver.get("https://finance.yahoo.com");
+			driver.manage().window().maximize();
+			driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+			Actions actions = new Actions(driver);
+			actions.sendKeys(Keys.PAGE_DOWN).build().perform();
+			WebElement ele = driver.findElement(By.xpath("//span[text()='More']"));
+			Actions builder=new Actions(driver);
+			builder.moveToElement(ele).perform();
+			driver.findElement(By.xpath("(//a[contains(text(),'Crypto')])[3]")).click();
+			List<WebElement> cryptoNames = driver.findElements(By.xpath("//table[contains(@class,'markets')]/tbody/tr/td[2]"));
+			int totalNames = cryptoNames.size();
+			System.out.println(totalNames);
+			for (WebElement list : cryptoNames) {
+				System.out.println(list.getText());
+			}
+			driver.close();
+			}
+
+}
