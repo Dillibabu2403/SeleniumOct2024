b32ef24a7b2ae13d6600ce042fd7b6e2751f46bd
Dillibabu2403 <118703394+Dillibabu2403@users.noreply.github.com>
2024-11-14 00:37:50 +0530

    Add files via upload
    baseclasswee5day3assignment

---
+++ week5/day3/BaseClass.java
@@ -0,0 +1,13 @@
+package stepdefinition;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class BaseClass {
+	
+public static ChromeDriver driver;
+public static Actions act;
+public static JavascriptExecutor js;
+
+}
